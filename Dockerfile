cc784bf3765d4eb4ca7638d2b537ef0a5e7773a4
thanhtoanththk <tp4302448@gmail.com>
2025-02-08 00:31:56 +0700

    update: update template website

--- Dockerfile
+++ Dockerfile
@@ -22,8 +22,15 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 RUN chmod -R 755 /app
 
+# Cài đặt biến môi trường FLASK_APP cho ứng dụng
+ENV FLASK_APP=app.py
+ENV FLASK_ENV=development
+
+# Kiểm tra nếu thư mục migrations không tồn tại thì tạo
+RUN if [ ! -d "migrations" ]; then flask db init; fi
+
 # Mở cổng 5000 để chạy Flask app
 EXPOSE 5001
 
 # Chạy ứng dụng Flask
-CMD ["python", "app.py"]
+CMD ["flask", "run", "--host=0.0.0.0", "--port=5001"]
--- app.sh
+++ app.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-# Start or Stop the Flask app based on the argument passed
-
 if [ "$1" == "start" ]; then
     echo "Starting the Flask app with Docker..."
     docker-compose up -d web
+    docker-compose exec web flask db migrate -m "Initial migration"
+    docker-compose exec web flask db upgrade
 elif [ "$1" == "stop" ]; then
     echo "Stopping the Flask app and removing Docker images..."
     docker-compose down
     docker image prune -f
     echo "Flask app stopped and Docker images removed."
+elif [ "$1" == "restart" ]; then
+    echo "Restarting the Flask app..."
+    docker-compose down
+    docker-compose up -d web
+    docker-compose exec web flask db migrate -m "Initial migration"
+    docker-compose exec web flask db upgrade
+    echo "Flask app restarted."
 else
-    echo "Usage: ./app.sh {start|stop}"
-    echo "Use 'start' to start the Flask app and 'stop' to stop it."
+    echo "Usage: ./app.sh {start|stop|restart}"
+    echo "Use 'start' to start the Flask app, 'stop' to stop it, and 'restart' to restart it."
 fi
